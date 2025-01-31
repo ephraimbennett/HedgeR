9db9b5bd231abab4d9022578ea46e4bd5ead1ff2
Ephraim Bennett <ephraimbennett330@gmail.com>
2025-01-30 19:28:03 -0500

    A new user model

--- venv_hedge/house_hedge/house_hedge/urls.py
+++ venv_hedge/house_hedge/house_hedge/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
-#handler404 = 'home.views.handler404'
+handler404 = 'home.views.handler404'
+LOGIN_REDIRECT_URL = "/"
