fccbab66be48de7723d53d8a98bdc95175f1ab2b
Ephraim Bennett <ephraimbennett330@gmail.com>
2025-02-05 18:11:08 -0500

    Redid the database

--- venv_hedge/house_hedge/home/admin.py
+++ venv_hedge/house_hedge/home/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from .models import Member
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(Member)
+class MemberAdmin(UserAdmin):
+    # Fields to display in the list view
+    list_display = ('email', 'phone', 'is_staff', 'is_superuser')
+    
+    # Fields to include in the add/edit forms
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('phone',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),)
+    
+    # Fields to include in the add user form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'phone', 'is_staff', 'is_superuser'),
+        }),
+    )
+    
+    # Ordering for the list view
+    ordering = ('email',)
+
+    # Display 'joined_date' as a read-only field
+    readonly_fields = ('joined_date',)
+
+admin.site.register(Member, MemberAdmin)
