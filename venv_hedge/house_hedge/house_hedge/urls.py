2052b1563c53ec4104af858ac61f87d4ddcb17b2
Ephraim Bennett <ephraimbennett330@gmail.com>
2025-02-07 22:02:52 -0500

    Removed the Members app

--- venv_hedge/house_hedge/house_hedge/urls.py
+++ venv_hedge/house_hedge/house_hedge/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('members.urls')),
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
